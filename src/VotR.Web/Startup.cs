68ffb522896cab943ee0830635fe03d01eb67bb2
t0bbz0n <tobias.trunehag@gmail.com>
2015-05-17 20:00:46 +0200

    X

--- src/VotR.Web/Startup.cs
+++ src/VotR.Web/Startup.cs
@@ -1,20 +1,13 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
 using Microsoft.Framework.DependencyInjection;
+using System;
+using Microsoft.AspNet.Diagnostics.Entity;
 
 namespace VotR.Web
 {
     public class Startup
     {
-        /*public Startup(IHostingEnvironment env)
-        {
-            // Setup configuration sources.
-            Configuration = new Configuration()
-                .AddJsonFile("config.json")
-                .AddEnvironmentVariables();
-        }
-
-        public IConfiguration Configuration { get; set; }*/
-
         // This method gets called by the runtime.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -31,19 +24,12 @@ namespace VotR.Web
             services.AddMvc();
 
             //services.AddTransient<TodoList.Services.StatisticsService>();
-            // Uncomment the following line to add Web API servcies which makes it easier to port Web API 2 controllers.
-            // You need to add Microsoft.AspNet.Mvc.WebApiCompatShim package to project.json
-            // services.AddWebApiConventions();
 
         }
 
         // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app)
         {
-            // Configure the HTTP request pipeline.
-            // Add the console logger.
-            //loggerfactory.AddConsole();
-
             // Add the following to the request pipeline only in development environment.
             /*if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
             {
@@ -71,9 +57,6 @@ namespace VotR.Web
                     name: "default",
                     template: "{controller}/{action}/{id?}",
                     defaults: new { controller = "Home", action = "Index" });
-
-                // Uncomment the following line to add a route for porting Web API 2 controllers.
-                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
             });
         }
     }
