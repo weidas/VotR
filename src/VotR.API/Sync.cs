668cfc327a699b6eb31cb1396dbdec947b88e97a
t0bbz0n <tobias.trunehag@gmail.com>
2015-05-17 19:45:03 +0200

    Testcontroller samt lite annat för att det skulle fungera.

---
+++ src/VotR.API/Sync.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotR.API
+{
+    public static class Sync
+    {
+        public static void SyncSystemBolaget()
+        {
+            
+        }
+    }
+}
---
+++ src/VotR.Web/Models/HomeModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotR.Web.Models
+{
+    public class HomeModel
+    {
+    }
+}
