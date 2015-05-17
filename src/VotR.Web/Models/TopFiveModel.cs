c4c92ed195892a5711741cfcc70f3cbaae35998c
t0bbz0n <tobias.trunehag@gmail.com>
2015-05-17 21:50:24 +0200

    Z

---
+++ src/VotR.Web/Models/TopFiveModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace VotR.Web.Models
+{
+    public class TopFiveModel
+    {
+        public int Position { get; set; }
+        //[DisplayName("Name")]
+        public string Name { get; set; }
+
+
+    }
+}
